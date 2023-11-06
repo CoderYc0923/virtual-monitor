d976b0b0b2f76945c429cdcfa766908f0823ae33
yechao <yechao@sucsoft.com>
2023-11-06 15:10:31 +0800

    feat: 项目初始化

---
+++ scripts/build.ts
@@ -0,0 +1,109 @@
+import path from 'path'
+import assert from 'assert'
+import { execSync as exec } from 'child_process'
+import fs from 'fs-extra'
+import fg from 'fast-glob'
+import consola from 'consola'
+import { packages } from '../meta/packages'
+import { version } from '../package.json'
+
+const rootDir = path.resolve(__dirname, '..')
+const watch = process.argv.includes('--watch')
+
+const FILES_COPY_ROOT = ['LICENSE']
+const FILES_COPY_LOCAL = ['README.md', 'index.json', '*.cjs', '*.mjs', '*.d.ts']
+
+assert(process.cwd() !== __dirname)
+
+/**
+ * 将打包后的 dist 文件进行二次处理 (复制一些公共文件进 dist 包以及文件的移动)
+ */
+async function buildMetaFiles() {
+  for (const { name, moduleJs, iife } of packages) {
+    const packageRoot = path.resolve(__dirname, '..', 'packages', name)
+    const packageDist = path.resolve(packageRoot, 'dist')
+
+    // ----- 复制 core README -----
+    if (name === 'core') {
+      await fs.copyFile(
+        path.join(rootDir, 'README.md'),
+        path.join(packageDist, 'README.md')
+      )
+    }
+
+    // ----- 复制 LICENSE 证书 -----
+    for (const file of FILES_COPY_ROOT) {
+      await fs.copyFile(path.join(rootDir, file), path.join(packageDist, file))
+    }
+
+    // ----- 将部分文件放入dist包 -----
+    const files = await fg(FILES_COPY_LOCAL, { cwd: packageRoot })
+    for (const file of files) {
+      await fs.copyFile(
+        path.join(packageRoot, file),
+        path.join(packageDist, file)
+      )
+    }
+
+    // ----- 更改本地引用版本 -----
+    const packageJSON = await fs.readJSON(
+      path.join(packageRoot, 'package.json')
+    )
+    // 当子类包互相引用时，要手动更改其版本（不改的话则是 workspace）
+    for (const key of Object.keys(packageJSON.dependencies || {})) {
+      if (key.startsWith('@web-tracing/')) {
+        packageJSON.dependencies[key] = version
+      }
+    }
+    // 因为只有在打包的时候才用这些，开发时需要 ./dist/index.mjs 才能正常引用
+    // 更改 exports、types、main、module、unpkg、jsdelivr（./dist/index.mjs => ./index.mjs）
+    packageJSON.types = './index.d.ts'
+    packageJSON.main = moduleJs ? './index.mjs' : './index.cjs'
+    packageJSON.module = './index.mjs'
+    if (iife !== false) {
+      packageJSON.unpkg = './index.iife.min.js'
+      packageJSON.jsdelivr = './index.iife.min.js'
+    }
+    packageJSON.exports = {
+      ...packageJSON.exports,
+      '.': {
+        import: './index.mjs',
+        require: './index.cjs',
+        types: './index.d.ts'
+      },
+      './*': './*'
+    }
+
+    await fs.writeJSON(path.join(packageDist, 'package.json'), packageJSON, {
+      spaces: 2
+    })
+  }
+}
+
+async function build() {
+  consola.info('Clean up')
+
+  exec('pnpm run clean', { stdio: 'inherit' })
+
+  exec(`pnpm run build:rollup${watch ? '-watch' : ''}`, {
+    stdio: 'inherit'
+  })
+
+  consola.info('build:types')
+
+  exec('pnpm run build:types', { stdio: 'inherit' })
+
+  await buildMetaFiles()
+}
+
+async function cli() {
+  try {
+    await build()
+  } catch (e) {
+    process.exit(1)
+  }
+}
+
+export { build }
+
+if (require.main === module) cli()
---
+++ scripts/utils.ts
@@ -0,0 +1,163 @@
+import { join, resolve } from 'path'
+import fs from 'fs-extra'
+import fg from 'fast-glob'
+import { $fetch } from 'ohmyfetch'
+import { packages } from '../meta/packages'
+
+export const DIR_ROOT = resolve(__dirname, '..')
+export const DIR_SRC = resolve(__dirname, '../packages')
+export const GUID_CATE_SRC = resolve(__dirname, '../packages/guide')
+const DIR_TYPES = resolve(__dirname, '../types/packages')
+
+/**
+ * 获取 ts 声明
+ * @param pkg 包类别
+ * @param name 包名
+ * @returns
+ */
+export async function getTypeDefinition(
+  pkg: string,
+  name: string
+): Promise<string | undefined> {
+  const typingFilepath = join(DIR_TYPES, `${pkg}/${name}/index.d.ts`)
+
+  if (!fs.existsSync(typingFilepath)) return
+
+  let types = await fs.readFile(typingFilepath, 'utf-8')
+
+  if (!types) return
+
+  // clean up types
+  types = types
+    .replace(/import\(.*?\)\./g, '')
+    .replace(/import[\s\S]+?from ?["'][\s\S]+?["']/g, '')
+    .replace(/export {}/g, '')
+
+  const prettier = await import('prettier')
+  return prettier
+    .format(types, {
+      semi: false,
+      parser: 'typescript'
+    })
+    .trim()
+}
+
+export function uniq<T extends any[]>(a: T) {
+  return Array.from(new Set(a))
+}
+
+export function replacer(
+  code: string,
+  value: string,
+  key: string,
+  insert: 'head' | 'tail' | 'none' = 'none'
+) {
+  const START = `<!--${key}_STARTS-->`
+  const END = `<!--${key}_ENDS-->`
+  const regex = new RegExp(`${START}[\\s\\S]*?${END}`, 'im')
+
+  const target = value ? `${START}\n${value}\n${END}` : `${START}${END}`
+
+  if (!code.match(regex)) {
+    if (insert === 'none') return code
+    else if (insert === 'head') return `${target}\n\n${code}`
+    else return `${code}\n\n${target}`
+  }
+
+  return code.replace(regex, target)
+}
+
+export async function updateCountBadge(indexes: any) {
+  const functionsCount = indexes.functions.filter(i => !i.internal).length
+  const url = `https://img.shields.io/badge/-${functionsCount}%20functions-13708a`
+  const data = await $fetch(url, { responseType: 'text' })
+  await fs.writeFile(
+    join(DIR_ROOT, 'packages/public/badge-function-count.svg'),
+    data,
+    'utf-8'
+  )
+}
+
+/**
+ * 填充 目标子包下的 index.ts 文件的导入导出
+ */
+export async function updateImport(packages) {
+  for (const { dir } of Object.values(packages) as any) {
+    const files = await fg('*', {
+      // onlyDirectories: true, // 只获取目录
+      cwd: `${dir}/src`,
+      ignore: ['_*', 'dist', 'node_modules']
+    })
+
+    const imports: string[] = files
+      .sort()
+      .map(name => name.split('.')[0])
+      .map(name => `export * from './src/${name}'`)
+
+    await fs.writeFile(join(dir, 'index.ts'), `${imports.join('\n')}\n`)
+
+    // await fs.remove(join(dir, 'index.mjs'))
+  }
+}
+
+/**
+ * 更改每个子包的 packages
+ */
+export async function updatePackageJSON() {
+  const { version } = await fs.readJSON('package.json')
+
+  for (const {
+    name,
+    description,
+    author,
+    iife,
+    keywords,
+    moduleJs
+  } of packages) {
+    const packageDir = join(DIR_SRC, name)
+    const packageJSONPath = join(packageDir, 'package.json')
+    const packageJSON = await fs.readJSON(packageJSONPath)
+
+    packageJSON.version = version
+    packageJSON.description = description || packageJSON.description
+    packageJSON.author =
+      author || '凡人 <https://gitee.com/ye_chao111>'
+    packageJSON.bugs = {
+      url: 'https://gitee.com/ye_chao111/virtual-monitor/issues'
+    }
+    packageJSON.homepage = 'https://gitee.com/ye_chao111/virtual-monitor#readme'
+    packageJSON.repository = {
+      type: 'git',
+      url: 'git+https://gitee.com/ye_chao111/virtual-monitor.git',
+      directory: `packages/${name}`
+    }
+    packageJSON.types = './dist/index.d.ts'
+    packageJSON.main = moduleJs ? './dist/index.mjs' : './dist/index.cjs'
+    packageJSON.module = './dist/index.mjs'
+    if (iife !== false) {
+      packageJSON.unpkg = './dist/index.iife.min.js'
+      packageJSON.jsdelivr = './dist/index.iife.min.js'
+    }
+    packageJSON.exports = {
+      ...packageJSON.exports,
+      '.': {
+        import: './dist/index.mjs',
+        require: './dist/index.cjs',
+        types: './dist/index.d.ts'
+      },
+      './*': './*'
+    }
+    if (keywords) {
+      packageJSON.keywords = [...keywords]
+    }
+
+    await fs.writeJSON(packageJSONPath, packageJSON, { spaces: 2 })
+  }
+}
+
+export function isValidKey(
+  key: string | number | symbol,
+  object: object
+): key is keyof typeof object {
+  return key in object
+}
